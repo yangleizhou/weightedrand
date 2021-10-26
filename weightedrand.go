1a8ff00a585d6945d008b2377ac96826f9fe812a
yangleizhou <yangleizhou@xunlei.com>
2021-10-26 18:05:00 +0800

    init

---
+++ weightedrand.go
@@ -0,0 +1,75 @@
+// Package weightedrand contains a performant data structure and algorithm used
+// to randomly select an element from some kind of list, where the chances of
+// each element to be selected not being equal, but defined by relative
+// "weights" (or probabilities). This is called weighted random selection.
+//
+// There is an existing Go library that has a generic implementation of this as
+// github.com/jmcvetta/randutil, which optimizes for the single operation case.
+// In contrast, this package creates a presorted cache optimized for binary
+// search, allowing repeated selections from the same set to be significantly
+// faster, especially for large data sets.
+package weightedrand
+
+import (
+	"math/rand"
+	"sort"
+)
+
+// Choice is a generic wrapper that can be used to add weights for any object
+type Choice struct {
+	Item     interface{}
+	Weight   uint
+	Selected bool
+}
+
+// A Chooser caches many possible Choices in a structure designed to improve
+// performance on repeated calls for weighted random selection.
+type Chooser struct {
+	data   []Choice
+	totals []int
+	max    int
+}
+
+// NewChooser initializes a new Chooser consisting of the possible Choices.
+func NewChooser(cs ...Choice) Chooser {
+	sort.Slice(cs, func(i, j int) bool {
+		return cs[i].Weight < cs[j].Weight
+	})
+	totals := make([]int, len(cs))
+	runningTotal := 0
+	for i, c := range cs {
+		runningTotal += int(c.Weight)
+		totals[i] = runningTotal
+	}
+	return Chooser{data: cs, totals: totals, max: runningTotal}
+}
+
+// Pick returns a single weighted random Choice.Item from the Chooser.
+func (chs Chooser) Pick() interface{} {
+	r := rand.Intn(chs.max) + 1
+	i := sort.SearchInts(chs.totals, r)
+	return chs.data[i].Item
+}
+
+// Pick several without duplication.
+func (chs Chooser) PickN(n int) []interface{} {
+	ret := make([]interface{}, 0)
+	length := 0
+	usingTotal := 0
+
+	for {
+		r := rand.Intn(chs.max) + 1
+		i := sort.SearchInts(chs.totals, r)
+		if !chs.data[i].Selected {
+			ret = append(ret, chs.data[i].Item)
+			chs.data[i].Selected = true
+			length += 1
+			usingTotal += int(chs.data[i].Weight)
+			if length >= n || float64(usingTotal)/float64(chs.max) > 0.75 {
+				break
+			}
+		}
+	}
+
+	return ret
+}
